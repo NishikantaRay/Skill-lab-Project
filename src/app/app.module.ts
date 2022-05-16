529b6deeb79f248293b5f6a0ed5de92b67b63ac2
Nishikantaray <nishikantaray1@gmail.com>
2022-05-17 00:20:42 +0530

    Added home footer login signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './home/login/login.component';
+import { SignupComponent } from './home/signup/signup.component';
+import { HomeComponent } from './home/home/home.component';
+import { FooterComponent } from './home/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
